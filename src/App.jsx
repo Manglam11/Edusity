9a927a649f0da1eb116c1737e3b67fdbbe7904c1
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-08-06 23:01:06 +0530

    campus section ho gya.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
-import Hero from './components/Hero/Hero'
-import Navbar from './components/Navbar/Navbar'
-import Programs from './components/Programs/Programs'
-import Title from './components/Title/Title'
-import About from './components/About/About'
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
+import About from "./components/About/About";
+import Campus from "./components/Campus/Campus";
 
 const App = () => {
     return (
@@ -15,11 +16,11 @@ const App = () => {
                 <Title subTitle="Our PROGRAM" title="What We Offer" />
                 <Programs />
                 <About />
-
+                <Title subTitle="Gallary" title="Campus Photos" />
+                <Campus />
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default App
+export default App;
