c28191b9eda334f715934823bef9be71209c997a
MANGLAM DUBEY <manglamdubey11@gmail.com>
2024-08-06 22:42:42 +0530

    suru k teen section ho gye.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
+import About from './components/About/About'
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
             <div className="container">
                 <Title subTitle="Our PROGRAM" title="What We Offer" />
                 <Programs />
+                <About />
 
             </div>
 
